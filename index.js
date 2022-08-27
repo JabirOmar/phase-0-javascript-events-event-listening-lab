b539876e78125165e7b9266e2ef37f6bb006e7f1
Jabir Omar <jabiromar400@gmail.com>
2022-08-27 21:51:23 +0300

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const input = document.getElementById('input')
+
 function addingEventListener() {
+    const input = document.getElementById('input')
+    input.addEventListener('click', clickAlart)
+    function clickAlart(){
+        alert('I was clicked!')
+    }
 }
+addingEventListener()
